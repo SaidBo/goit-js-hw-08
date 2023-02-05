7a3c173b3876ae2d7f08fd29f7175118ebc23b4a
SaidBo <bsblfc@gmail.com>
2023-02-05 15:12:13 +0200

    2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+        <img 
+         class="gallery__image" 
+         src="${preview}" 
+         alt="${description}" />
+    </a>
+`;
+    })
+    .join("");
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
