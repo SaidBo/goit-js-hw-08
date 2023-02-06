1e87001bbcb7bb09557e1a8cce44a200a3329ddc
SaidBo <bsblfc@gmail.com>
2023-02-06 13:54:46 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  //   textarea: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  console.log(formData);
+
+  const formDataJSON = JSON.stringify(formData);
+
+  localStorage.setItem('STORAGE_KEY', formDataJSON);
+}
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+
+  localStorage.removeItem('STORAGE_KEY');
+}
+
+function populateTextarea() {
+  const enteredData = JSON.parse(localStorage.getItem('STORAGE_KEY'));
+
+  if (enteredData) {
+    refs.form.elements.email.value = enteredData.email;
+    refs.form.elements.message.value = enteredData.message;
+  }
+}
 
